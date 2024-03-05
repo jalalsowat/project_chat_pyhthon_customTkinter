7cdf339f53edbbb677f443cfd788ed7e8c9814dc
Jalal0303 <Jalal0303@users.noreply.github.com>
2024-03-06 00:43:49 +0300

    add and design Basic Frame

--- chat_python_customTkinter.py
+++ chat_python_customTkinter.py
@@ -8,6 +8,9 @@ class App(ctk.CTk):
         super().__init__()
         self.geometry("1024x720")
         self.title("Chat App")
+
+        self.list_person =[]
+        self.get_chats_Net()
       
         self.grid_rowconfigure(0,weight=0)
         self.grid_rowconfigure(1,weight=1)
@@ -26,6 +29,8 @@ class App(ctk.CTk):
         self.leftFrame = ctk.CTkScrollableFrame(self,fg_color="#403F3F",corner_radius=0)
         self.leftFrame.grid(row=1,column=0,sticky="nswe")
 
+        self.leftFrame.grid_columnconfigure(0,weight=1)
+
         self.rightFrame = ctk.CTkFrame(self,fg_color="transparent",corner_radius=0)
         self.rightFrame.grid(row=1,column=1,sticky="nswe")
 
@@ -33,7 +38,7 @@ class App(ctk.CTk):
         self.rightFrame.grid_rowconfigure((0,2),weight=0)
         self.rightFrame.grid_rowconfigure(1,weight=1)
 
-        self.headerFrame = ctk.CTkFrame(self.rightFrame,corner_radius=0,fg_color="#212222")
+        self.headerFrame = ctk.CTkFrame(self.rightFrame,corner_radius=0,fg_color="#2B1123")
         self.headerFrame.grid(row=0,column=0,sticky="nswe")
 
         self.headerFrame.grid_rowconfigure(0,weight=1)
@@ -51,11 +56,60 @@ class App(ctk.CTk):
         self.footerFrame.grid_columnconfigure(0,weight=1)
         self.footerFrame.grid_columnconfigure(1,weight=0)
 
-        self.ent = ctk.CTkEntry(self.footerFrame,placeholder_text="write here")
+        self.ent = ctk.CTkEntry(self.footerFrame,placeholder_text="Write here")
         self.ent.grid(row=0,column=0,padx=5,pady=10,sticky="nswe")
 
         self.btn_send = ctk.CTkButton(self.footerFrame,text="Send",font=("arial",18))
         self.btn_send.grid(row=0,column=1,padx=5,pady=10)
+
+        self.load_chats(self.list_person)
+
+    def get_chats_Net(self):
+        self.list_person=[
+       {
+           "name": "jalal",
+           "info":"yes",
+           "time": "8:26 PM"
+       },
+        {
+           "name": "Ahmed",
+           "info":"Yep",
+           "time": "9:46 PM"
+       },
+
+        ]
+
+
+    def load_chats(self,ListPerson):
+        for i in range(len(ListPerson)):
+            self.basicFrame = ctk.CTkFrame(self.leftFrame,corner_radius=0,fg_color="transparent",height=100)
+            self.basicFrame.grid(row=i,column=0,sticky="nswe",pady=5)
+
+            self.basicFrame.rowconfigure(0,weight=1)
+            self.basicFrame.columnconfigure(0,weight=0)
+            self.basicFrame.columnconfigure(1,weight=1)
+
+            self.lbl_ch = ctk.CTkLabel(self.basicFrame, text=ListPerson[i]["name"][0], corner_radius=10,font=("arial",20,"bold"),fg_color="#1AA1BF",height=40,width=40)
+            self.lbl_ch.grid(row=0,column=0,padx=10,pady=10,sticky="w")
+
+            self.frameInfo = ctk.CTkFrame(self.basicFrame,corner_radius=0,fg_color="transparent")
+            self.frameInfo.grid(row=0,column=1,sticky="nswe")
+
+            self.frameInfo.grid_columnconfigure(0,weight=1)
+            self.frameInfo.grid_rowconfigure((1,2,3),weight=0)
+            self.frameInfo.grid_rowconfigure((0,4),weight=1)
+
+            self.lbl_name = ctk.CTkLabel(self.frameInfo, text=ListPerson[i]["name"], font=("arial",18,"bold"))
+            self.lbl_name.grid(row=1,column=0,sticky="w")
+
+            self.lbl_info = ctk.CTkLabel(self.frameInfo, text=ListPerson[i]["info"], font=("arial",15))
+            self.lbl_info.grid(row=2,column=0,sticky="w")
+
+            self.lbl_time = ctk.CTkLabel(self.frameInfo, text=ListPerson[i]["time"], font=("arial",15))
+            self.lbl_time.grid(row=3,column=0,sticky="w")
+
+            self.lbl_divider =ctk.CTkFrame(self.basicFrame,fg_color="#1AA1BF",height=3)
+            self.lbl_divider.grid(row=4,column=0,columnspan=2,sticky="nswe",padx=5,pady=5)
 
 
 
